ac8b4a973a07330346c91267a0dad23fabca988a
Erik Plachta <eplachta@gmail.com>
2022-01-27 17:45:53 -0500

    Add post model

---
+++ models/Post.js
@@ -0,0 +1,61 @@
+//------------------------------------------------------------------------------
+//-- Imports
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+//------------------------------------------------------------------------------
+//-- class
+
+// create our Post model
+class Post extends Model {
+
+};
+
+
+//------------------------------------------------------------------------------
+//-- Defining Table
+
+
+// create fields/columns for Post model
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      post_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            //-- makes sure url is actually a URL!
+            isURL: true
+        }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
+
+
+//------------------------------------------------------------------------------
+//-- Exports
+
+module.exports = Post;
